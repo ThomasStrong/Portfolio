7cd6ffdfb5e284cf718842fd07901a72873ad5fe
Tom Strong <strng_thms@yahoo.com>
2022-08-19 15:45:09 -0400

    Nav set up and working

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,53 @@
 import React from 'react';
 import './nav.css';
+import { GoHome } from 'react-icons/go';
+import { FaRegUser } from 'react-icons/fa';
+import { BiBook } from 'react-icons/bi';
+import { VscBriefcase } from 'react-icons/vsc';
+import { TiMessages } from 'react-icons/ti';
+import { useState } from 'react';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [activeNav, setActiveNav] = useState('#');
+  return (
+    <nav>
+      <a
+        href='#'
+        onClick={() => setActiveNav('#')}
+        className={activeNav === '#' ? 'active' : ''}
+      >
+        <GoHome />
+      </a>
+      <a
+        href='#about'
+        onClick={() => setActiveNav('#about')}
+        className={activeNav === '#about' ? 'active' : ''}
+      >
+        <FaRegUser />
+      </a>
+      <a
+        href='#experience'
+        onClick={() => setActiveNav('#experience')}
+        className={activeNav === '#experience' ? 'active' : ''}
+      >
+        <BiBook />
+      </a>
+      <a
+        href='#portfolio'
+        onClick={() => setActiveNav('#portfolio')}
+        className={activeNav === '#portfolio' ? 'active' : ''}
+      >
+        <VscBriefcase />
+      </a>
+      <a
+        href='#contact'
+        onClick={() => setActiveNav('#contact')}
+        className={activeNav === '#contact' ? 'active' : ''}
+      >
+        <TiMessages />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
