b52c85f2c2cd5cb6dd11d096e9712d7fceb3ba3e
Tom Strong <strng_thms@yahoo.com>
2022-08-19 21:19:36 -0400

    Add tooltips to nav, working on portfolio styling

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,43 +6,55 @@ import { BiBook } from 'react-icons/bi';
 import { VscBriefcase } from 'react-icons/vsc';
 import { TiMessages } from 'react-icons/ti';
 import { useState } from 'react';
+import ReactTooltip from 'react-tooltip';
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#');
   return (
     <nav>
+      <ReactTooltip delayShow={800} />
       <a
         href='#'
+        data-tip='Home'
         onClick={() => setActiveNav('#')}
         className={activeNav === '#' ? 'active' : ''}
+        rel='noreferrer'
       >
         <GoHome />
       </a>
       <a
         href='#about'
+        data-tip='About'
         onClick={() => setActiveNav('#about')}
         className={activeNav === '#about' ? 'active' : ''}
+        rel='noreferrer'
       >
         <FaRegUser />
       </a>
       <a
         href='#experience'
+        data-tip='Experience'
         onClick={() => setActiveNav('#experience')}
         className={activeNav === '#experience' ? 'active' : ''}
+        rel='noreferrer'
       >
         <BiBook />
       </a>
       <a
         href='#portfolio'
+        data-tip='Portfolio'
         onClick={() => setActiveNav('#portfolio')}
         className={activeNav === '#portfolio' ? 'active' : ''}
+        rel='noreferrer'
       >
         <VscBriefcase />
       </a>
       <a
         href='#contact'
+        data-tip='Contact'
         onClick={() => setActiveNav('#contact')}
         className={activeNav === '#contact' ? 'active' : ''}
+        rel='noreferrer'
       >
         <TiMessages />
       </a>
