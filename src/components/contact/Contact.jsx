01ca153b4957a323ceef2e464e57b841b002a649
Tom Strong <strng_thms@yahoo.com>
2022-08-21 16:05:09 -0400

    Contact  form built out and connecting to emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './contact.css';
 import { MdOutlineEmail } from 'react-icons/md';
+import { AiOutlinePhone } from 'react-icons/ai';
 
 const Contact = () => {
   return (
@@ -11,14 +12,39 @@ const Contact = () => {
       <div className='container contact__container'>
         <div className='contact__options'>
           <article className='contact__option'>
-            <MdOutlineEmail />
+            <MdOutlineEmail className='contact__option-icon' />
             <h4>Email</h4>
             <h5>strongertoms@gmail.com</h5>
-            <a href='mailto:strongertoms@gmail.com'></a>
+            <a href='mailto:strongertoms@gmail.com' target='_blank'>
+              Send a Message
+            </a>
+          </article>
+          <article className='contact__option'>
+            <AiOutlinePhone className='contact__option-icon' />
+            <h4>Phone</h4>
+            <h5>720.304.5826</h5>
+            {/* <a href='mailto:strongertoms@gmail.com'>Send a Message</a> */}
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action=''></form>
+        <form action=''>
+          <input
+            type='text'
+            name='name'
+            placeholder='Your Full Name'
+            required
+          />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea
+            name='message'
+            rows='7'
+            placeholder='Your Message'
+            required
+          ></textarea>
+          <button type='submit' className='btn btn-primary'>
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
