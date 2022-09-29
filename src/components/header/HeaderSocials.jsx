67d532714c743723d7629a0c2d09532ba4cd54a8
Tom Strong <strng_thms@yahoo.com>
2022-09-29 14:33:37 -0400

    Add social icon and updates

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -3,6 +3,7 @@ import { BsLinkedin } from 'react-icons/bs';
 import { FaGithub } from 'react-icons/fa';
 import { FaFreeCodeCamp } from 'react-icons/fa';
 import { SiExercism } from 'react-icons/si';
+import { SiPluralsight } from 'react-icons/si';
 
 const HeaderSocials = () => {
   return (
@@ -21,7 +22,18 @@ const HeaderSocials = () => {
       >
         <FaGithub />
       </a>
-      <a href='https://www.freecodecamp.org/' target='_blank' rel='noreferrer'>
+      <a
+        href='https://app.pluralsight.com/profile/thomas-strong'
+        target='_blank'
+        rel='noreferrer'
+      >
+        <SiPluralsight />
+      </a>
+      <a
+        href='https://www.freecodecamp.org/FrothyDev'
+        target='_blank'
+        rel='noreferrer'
+      >
         <FaFreeCodeCamp />
       </a>
       <a href='https://exercism.org/' target='_blank' rel='noreferrer'>
